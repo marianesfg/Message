f312fcbbab7d085df6ef5fddca84d6d745c12f45
Mariane <mariane.sfgodoy@gmail.com>
2020-12-13 17:48:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Threading.Tasks;
+
+namespace Message
+{
+    class Program
+    {
+        public static void Main()
+        {
+            PrintMessage("eae");
+
+            Console.ReadKey();
+        }
+
+
+        public static async Task<string> Nova()
+        {
+            return "New message";
+        }
+
+        static async void PrintMessage(string name)
+        {
+            Console.WriteLine("{0}", await Task.Run(() => GetMessage(name)));
+                             
+            var res1 = await Nova();
+            Console.WriteLine(res1);
+        }
+
+        static string GetMessage(string name)
+        {
+            return "Hi, " + name;
+        }
+    }
+}
